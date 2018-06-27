f43809e2d5e1ebc6616ad53c598c74158ece3162
Serge Victor <ser@users.noreply.github.com>
2018-06-27 18:19:52 +0700

    improved merchant automation script

--- contrib/electrum-as-merchant-server-installation.sh
+++ contrib/electrum-as-merchant-server-installation.sh
@@ -2,14 +2,10 @@
 # (C) Serge Victor 2018, MIT license
 # 
 # This script installs and configures Electrum wallet as a seed-less read-only merchant daemon.
+# Contact me on IRC (Freenode): channel: #electrum --> user: ser
 #
-# Notes: 
-#        Electrum Dash will be added if reaches version 3: 
-#        https://github.com/akhavr/electrum-dash
-#
-#        Electron Cash will be added if it follows Electrum more strictly:
-#        https://github.com/fyookball/electrum
-#        (missing merchant related stuff as for example getfeerate)
+# On the observation list, not ready to be used yet:
+# zcash support, https://github.com/zcash-community/electrum-zec
 
 function jsonValue() {
 	KEY=$1
@@ -17,7 +13,7 @@ function jsonValue() {
 }
 
 ####
-# apt install python3-pip python3-wheel python3-setuptools python3-dev virtualenvwrapper pkg-config
+# apt install python3-pip python3-wheel python3-setuptools python3-dev virtualenvwrapper pkg-config dh-autoreconf libssl-dev libusb-1.0-0-dev libudev-dev protobuf-compiler pyqt5-dev-tools
 ####
 
 echo ""
@@ -27,7 +23,8 @@ echo "as a merchant daemon with websockets activated."
 echo ""
 echo "Before you start using this script, you need to prepare your system."
 echo "1) As root, install mandatory packages:"
-echo "   # apt install python3-pip python3-wheel python3-setuptools python3-dev pkg-config dh-autoreconf libssl-dev libusb-1.0-0-dev libudev-dev"
+echo "   # apt install python3-pip python3-wheel python3-setuptools python3-dev pkg-config \
+       	dh-autoreconf libssl-dev libusb-1.0-0-dev libudev-dev protobuf-compiler pyqt5-dev-tools"
 echo "2) As root, Unblock firewall to access Electrum servers ports, usually:"
 echo "   50000-50010 and/or 51000-51010 (testnet)"
 echo "   After running this script you will also need to unblock incoming traffic"
@@ -47,6 +44,10 @@ OPTIONS=(
 	"Electrum Bitcoin Testnet Original"
 	"Electrum Litecoin"
 	"Electrum Litecoin Testnet"
+	"Electrum Dash"
+	"Electrum Dash Testnet"
+	"Bitcoin Cash"
+	"Bitcoin Cash Testnet"
 	)
 
 echo "Select which Electrum flavour do you need:"
@@ -57,6 +58,10 @@ select option in "${OPTIONS[@]}"; do
 		2) export ELECTRUM="EBOT"; export GIT="https://github.com/spesmilo/electrum"; break ;;
 		3) export ELECTRUM="EL"; export GIT="https://github.com/pooler/electrum-ltc"; break ;;
 		4) export ELECTRUM="ELT"; export GIT="https://github.com/pooler/electrum-ltc"; break ;;
+		5) export ELECTRUM="ED"; export GIT="https://github.com/akhavr/electrum-dash"; break ;;
+		6) export ELECTRUM="EDT"; export GIT="https://github.com/akhavr/electrum-dash"; break ;;
+		7) export ELECTRUM="EC"; export GIT="https://github.com/fyookball/electrum/"; break ;;
+		8) export ELECTRUM="ECT"; export GIT="https://github.com/fyookball/electrum/"; break ;;
 	esac
 done
 
@@ -130,19 +135,36 @@ if [ $ELECTRUM = "EBO" ] || [ $ELECTRUM = "EBOT" ]; then
 elif [ $ELECTRUM = "EL" ] || [ $ELECTRUM = "ELT" ]; then
 	ln -s electrum-ltc electrum || true
 	ln -s ~/.electrum-ltc ~/.electrum || true
+elif [ $ELECTRUM = "ED" ] || [ $ELECTRUM = "EDT" ]; then
+	ln -s electrum-dash electrum || true
+	ln -s ~/.electrum-dash ~/.electrum || true
+elif [ $ELECTRUM = "EC" ] || [ $ELECTRUM = "ECT" ]; then
+	ln -s electron-cash electrum || true
+	ln -s ~/.electron-cash ~/.electrum || true
 fi
 
-if [ $ELECTRUM = "EBOT" ] || [ $ELECTRUM = "ELT" ]; then
+if [ $ELECTRUM = "EBOT" ] || [ $ELECTRUM = "ELT" ] || [ $ELECTRUM = "EDT" ] || [ $ELECTRUM = "ECT" ] ; then
 	echo ""
 	echo "You want to operate on Testnet."
 	TESTNET="--testnet"
 	NETWORK="--network=testnet"
 fi
 
+# cutting only hostname from URI provided
+WSNAME=`echo $INTERNET_URI | cut -d/ -f3`
+
 # Creating requests directory
+
 mkdir ~/"$USER" || true
 
-#python3 ./electrum $TESTNET setconfig proxy "socks5:10.74.1.2:9050::"
+if [ $ELECTRUM = "ECT" ]; then
+	# Bitcoin cash default testnet servers are nonexistent
+	python3 ./electrum $TESTNET setconfig server "bcht.random.re:53002:s"
+fi
+
+# if you use a proxy, as me:
+python3 ./electrum $TESTNET setconfig proxy "socks5:10.73.1.5:9050::"
+
 echo "Accessing your read-only wallet..."
 python3 ./electrum $TESTNET restore $WALLET
 echo "Configuring Electrum daemon..."
@@ -154,10 +176,12 @@ python3 ./electrum $TESTNET setconfig websocket_server "0.0.0.0"
 python3 ./electrum $TESTNET setconfig url_rewrite "[ 'file:///home/$USER/$USER/', $INTERNET_URI ]"
 python3 ./electrum $TESTNET setconfig ssl_chain $SSL_CHAIN
 python3 ./electrum $TESTNET setconfig ssl_privkey $SSL_KEY
+python3 ./electrum $TESTNET setconfig websocket_server_announce $WSNAME
+# eventual additional config is up to you
 #python3 ./electrum $TESTNET setconfig
 
 echo "Running Electrum daemon for first time, to get a random RPC password..."
-# Faking a file to pass through electrum's hard warning.
+# Faking a file temporary to pass through electrum's hard warning.
 touch /home/$USER/$USER/index.html
 python3 ./electrum $TESTNET daemon start
 python3 ./electrum $TESTNET daemon stop
@@ -192,6 +216,7 @@ service=$(cat <<EOF
 [Unit]
 Description=Electrum $USER Server
 After=multi-user.target
+After=network.target
 
 [Service]
 ExecStart=python3 /home/$USER/electrum/electrum $TESTNET daemon start
@@ -199,6 +224,14 @@ ExecStop=python3 /home/$USER/electrum/electrum $TESTNET daemon stop
 ExecStartPost=python3 /home/$USER/electrum/electrum daemon load_wallet
 Type=forking
 User=${USER}
+RestartSec=10s
+WorkingDirectory=/home/$USER/
+Restart=always
+# If you want to bind electrum to a ports below 1024 uncomment
+# the two values below
+###
+#CapabilityBoundingSet=CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_BIND_SERVICE
 
 [Install]
 WantedBy=multi-user.target
